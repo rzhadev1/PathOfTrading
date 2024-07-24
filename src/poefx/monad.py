da948ea4c5f95030e16a218d6af7e5937706ed78
Richard Z <richardz@10-21-150-22.dynapool.wireless.nyu.edu>
2024-07-23 22:58:02 -0400

    no supply variables, but working

---
+++ src/poefx/monad.py
@@ -0,0 +1,78 @@
+import pyomo.environ as pyo
+
+"""
+An implementation of a basic Pyomo monad, taken from https://mobook.github.io/MO-book/notebooks/appendix/functional-programming-pyomo.html
+"""
+
+class PyomoMonad:
+    """A simple monad class for handling Pyomo operations with error handling.
+    
+    This class is designed to simplify Pyomo operations by providing a way to
+    handle errors and chaining operations together. The bind method is used to
+    chain operations, and the __call__ method is used to execute the monad.
+    """
+
+    def __init__(self, value=None, effect=None, status=None):
+        """
+        Initialize the PyomoMonad instance.
+        
+        :param value: The value to be processed by the monad
+        :type value: Any
+        :param effect: A function to be applied on the value
+        :type effect: callable, optional
+        :param status: The current status of the monad, used for error handling
+        :type status: Any, optional
+        """
+        self.value = value
+        self.status = status
+        self.effect = effect
+
+    def bind(self, func):
+        """Bind a function to the current monad instance.
+        
+        :param func: The function to be bound to the current monad
+        :type func: callable
+        :return: A new PyomoMonad instance
+        :rtype: PyomoMonad
+        """
+        if self.status is not None:
+            return PyomoMonad(None, self.effect, self.status)
+
+        try:
+            result = func(self.value)
+            return PyomoMonad(result, self.effect, self.status)
+
+        except Exception as e:
+            return PyomoMonad(None, lambda value: print(self.status), status=e)
+
+    def __call__(self):
+        """Execute the monad by calling its effect if it exists, or return its value.
+        
+        :return: The result of executing the monad's effect or its value
+        :rtype: Any
+        """
+        if self.effect is not None:
+            return self.effect(self.value)
+        else:
+            return self.value
+
+    def __rshift__(self, func):
+        """Implement the '>>' operator for chaining operations.
+        
+        :param func: The function to be bound to the current monad
+        :type func: callable
+        :return: A new PyomoMonad instance
+        :rtype: PyomoMonad
+        """
+        return self.bind(func)
+    
+def Solver(s=None):
+    """ Given a string solver name, try to create a pyomo.environ.SolverFactory solver. """
+    if s is None:
+        _solver = lambda model : model.clone()
+    else:
+        def _solver(model):
+            new_model = model.clone()
+            pyo.SolverFactory(s).solve(new_model)
+            return new_model
+    return _solver
--- src/poefx/optimize.py
+++ src/poefx/optimize.py
@@ -1,9 +1,6 @@
 import pyomo.environ as pyo
 import pandas as pd
-
-solver = 'cbc'
-SOLVER = pyo.SolverFactory(solver)
-assert SOLVER.available(), f"Solver {solver} is not available."
+from monad import PyomoMonad, Solver
 
 def optimal_conversion(market_df : pd.DataFrame, from_currency : str, to_currency : str, 
                        from_currency_qty : int, num_trades : int, start_gold : int):
@@ -35,6 +32,10 @@ def optimal_conversion(market_df : pd.DataFrame, from_currency : str, to_currenc
 
     # the gold cost associated with each transaction
     gold_dict = {conversion : d['gold_cost'] for conversion, d in conversions.items()}
+    m.GOLD_COST = pyo.Param(m.CONV, initialize=gold_dict)
+
+    # the amount of gold on hand at a given trade t
+    m.g = pyo.Var(m.TRADES, domain=pyo.NonNegativeReals)
 
     # the amount of currency i on hand after trade t
     m.x = pyo.Var(m.CURR, m.TRADES, domain=pyo.NonNegativeReals)
@@ -42,36 +43,58 @@ def optimal_conversion(market_df : pd.DataFrame, from_currency : str, to_currenc
     # the amount of currency i committed to be converted to currency j in transaction t
     m.c = pyo.Var(m.CONV, m.TRANSACTIONS, domain=pyo.NonNegativeReals)
 
-    # start with some initial number of the starting currency
+    # the number of increments made for a conversion, which must be integer
+    m.incs = pyo.Var(m.CONV, m.TRANSACTIONS, domain=pyo.NonNegativeIntegers)
+
+    # start with initial number of the starting currency
     @m.Constraint(m.CURR)
     def initial_cond(m, i):
         if i == from_currency:
             return m.x[i, 0] == from_currency_qty
-        
         return m.x[i, 0] == 0
+    
+    # start with initial amount of gold
+    @m.Constraint()
+    def initial_gold(m):
+        return m.g[0] == start_gold
+    
+    # must trade in the correct increments
+    @m.Constraint(m.CONV, m.TRANSACTIONS)
+    def trade_increments(m, i, j, t):
+        return m.c[i,j,t] == m.incs[i,j,t] * m.MIN_INC[i,j]
+    
+    # gold balance is maintained after a round of transactions
+    @m.Constraint(m.TRANSACTIONS)
+    def gold_balance(m, t):
+        # the gold cost incurred from the transactions made
+        cost = sum(m.incs[i,j,t] * m.GOLD_COST[i,j] for (i,j) in m.CONV)
+        return m.g[t] == m.g[t-1] - cost
+    
+    # balances are maintained after a round of transactions
+    @m.Constraint(m.CURR, m.TRANSACTIONS)
+    def balance(m, j, t):
+        outgoing = sum(m.c[j, i, t] for i in m.CURR if i != j and (j,i) in m.CONV)
+        incoming = sum(m.RATES[(i, j)] * m.c[(i, j), t] for i in m.CURR if i != j and (i,j) in m.CONV)
+        return m.x[j, t] == m.x[j, t-1] - outgoing + incoming
     
     @m.Constraint(m.CURR, m.TRANSACTIONS)
     def no_shorting(m, j, t):
         return m.x[j, t-1] >= sum(m.c[j, i, t] for i in m.CURR if i != j and (j,i) in m.CONV)
     
-    # balances are maintained after a round of transactions
-    @m.Constraint(m.CURR, m.TRANSACTIONS)
-    def balances(m, j, t):
-        outgoing = sum(m.c[j, i, t] for i in m.CURR if i != j and (j,i) in m.CONV)
-        incoming = sum(m.RATES[(i, j)] * m.c[(i, j), t] for i in m.CURR if i != j and (i,j) in m.CONV)
-        return m.x[j, t] == m.x[j, t-1] - outgoing + incoming
-    
     # maximize the ending currency
     @m.Objective(sense=pyo.maximize)
-    def max_to_currency(m):
+    def conversion_amt(m):
         return m.x[to_currency, num_trades]
+    
+    # result call back
+    def result(m):
+        trades = {(curr_a, curr_b, step): m.c[curr_a, curr_b, step].value for curr_a,curr_b,step in m.c}
+        return m.conversion_amt(), trades
 
-    res = SOLVER.solve(m)
-
-    m.pprint()
-    print(m.x['Divine Orb', 3]())
-
+    return PyomoMonad(m, effect=result)
 
 if __name__ == '__main__':
     df = pd.read_csv('currency_market1.csv')
-    optimal_conversion(df, 'Chaos Orb', 'Divine Orb', 121, 3, 1000000)
+    amt, trades = (optimal_conversion(df, 'Chaos Orb', 'Divine Orb', 121, 3, 1000000) >> Solver('cbc'))()
+    print(trades)
+    print(amt)
